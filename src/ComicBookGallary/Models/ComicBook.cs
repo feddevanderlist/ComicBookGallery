415fe4a2816e8af51fcd62e3b427acff5cba1a0e
fedde <feddevanderlist@gmail.com>
2018-11-09 15:59:03 +0100

    created comicBook model

---
+++ src/ComicBookGallary/Models/ComicBook.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ComicBookGallary.Models
+{
+    public class ComicBook
+    {
+        public int Id { get; set; }
+        public string SeriesTitle { get; set; }
+        public int IssueNumber { get; set; }
+        public string DescriptionHtml { get; set; }
+        public Artist[] Artist { get; set; }
+        public bool Favorite { get; set; }
+
+        public string DisplayText
+        {
+            get { return SeriesTitle + IssueNumber; }
+        }
+
+        public string CoverImageFile
+        {
+            get { return SeriesTitle.Replace(" ", "-").ToLower() + "-" + IssueNumber + ".jpg"; }
+        }
+    }
+}
